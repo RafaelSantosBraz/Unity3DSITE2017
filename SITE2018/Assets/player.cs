53807110c3cef4603bb2760a09fe24ed06cca476
Rafael dos Santos Braz <rafinha.santos.braz@gmail.com>
2018-11-07 17:55:41 -0200

    minicurso 2018

---
+++ SITE2018/Assets/player.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    public float speed = 10;
+
+    // Use this for initialization
+    void Start()
+    {
+        gameObject.transform.position = new Vector3(0, 0, 0);
+    }
+
+    private void OnCollisionEnter(Collision c)
+    {
+        print("bateu" + c.gameObject.tag);
+        if (c.gameObject.tag == "enemy")
+        {
+            if (gameObject.transform.localScale.x >= c.gameObject.transform.localScale.x)
+            {
+                gameObject.transform.localScale += c.gameObject.transform.localScale;
+                Destroy(c.gameObject);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float x = transform.position.x;
+        float z = transform.position.z;
+        float y = transform.position.y;
+
+        // print(Input.GetKey(KeyCode.RightArrow));
+        //print(Input.GetAxis("Horizontal"));
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            gameObject.transform.position = new Vector3(x + speed * Time.deltaTime * Input.GetAxis("Horizontal"), y + speed * Time.deltaTime, z + speed * Time.deltaTime * Input.GetAxis("Vertical"));
+        }
+        else
+        {
+            gameObject.transform.position = new Vector3(x + speed * Time.deltaTime * Input.GetAxis("Horizontal"), y, z + speed * Time.deltaTime * Input.GetAxis("Vertical"));
+        }
+
+
+
+
+
+        //print(gameObject.transform.position.x);
+    }
+}
---
+++ SITE2018/Assets/spawn.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawn : MonoBehaviour
+{
+
+    public GameObject enemyPreFab;
+
+    // Use this for initialization
+    void Start()
+    {
+        InvokeRepeating("toSpawn", 1, 1);
+    }
+
+    void toSpawn()
+    {
+        //Instantiate(this, transform);
+        for (int i = 0; i < 100; i++)
+        {
+            Instantiate(enemyPreFab, transform);
+        }
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
